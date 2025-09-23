b7fef989095e2c59c4395adfd4834500008ed77b
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2025-09-23 01:20:46 +0000

    Publish a new version with Formula

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,15 +1,15 @@
 class Nexttrace < Formula
     desc "An open source visual route tracking CLI tool"
     homepage "https://github.com/nxtrace/NTrace-V1"
-    version "v1.4.3-beta.1"
-    url "https://github.com/nxtrace/NTrace-V1/archive/refs/tags/v1.4.3-beta.1.tar.gz"
-    sha256 "a35ca994839878b2502600b84134a3b90ff85b25b1609c11fcbfe692828535b2"
+    version "v1.4.3-beta.2"
+    url "https://github.com/nxtrace/NTrace-V1/archive/refs/tags/v1.4.3-beta.2.tar.gz"
+    sha256 "46d1a9b241c0e81ca29433743db5240b398b532b95ee8a8171f08bf86632119a"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/nxtrace/NTrace-core/config.Version=v1.4.3-beta.1' -s -w -checklinkname=0")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/nxtrace/NTrace-core/config.Version=v1.4.3-beta.2' -s -w -checklinkname=0")
     end
   
     test do
